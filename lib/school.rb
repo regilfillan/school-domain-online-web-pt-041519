8a96b8dc117d59e8f2d071e8397b7ff10a744b8e
Ryan Gilfillan <regilfillan@gmail.com>
2019-05-12 22:06:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,9 +6,9 @@ class School
   @roster={}
   end
   
-  def add_student(student_name, grade)
-   @roster[grade] ||= []
-  @roster[grade]<< student_name
- end
+ # def add_student(student_name, grade)
+ #  @roster[grade] ||= []
+#  @roster[grade]<< student_name
+ #end
 
 end
